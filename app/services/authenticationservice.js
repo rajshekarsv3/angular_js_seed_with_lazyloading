4f2ba60a3fcfdd208c0e4ceeedd16ed6518608e7
Rajshekar <rajshekarsv3@gmail.com>
2014-11-27 12:44:51 +0530

    Handled Validation

--- app/services/authenticationservice.js
+++ app/services/authenticationservice.js
@@ -5,7 +5,8 @@ factory('AuthService',function($http){
 	var authService = {
 		login: function(credentials) {
 			$http.post("http://localhost:3000/api/v1/sessions",{user:{email:credentials.username,password:credentials.password}})
-    			.success(function(response) {console.log(response);});
+    			.success(function(response) {console.log(response);})
+    			.error(function(response) {console.log(response)});
 		}
 	}
 	return authService;
